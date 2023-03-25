de3228569d3e24bf783254a39c0b4eb37cea0d20
ddulyas <120781735+ddulyas@users.noreply.github.com>
2023-03-26 00:43:02 +0500

    add auth middleware

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import userController from "../controllers/user-controller.js";
+import { body } from "express-validator";
+import authMiddleware from "../middleware/auth-middleware.js";
+
 
 const router = new Router()
 
-router.post('/registration', userController.registration)
+router.post('/registration',
+    body('email').isEmail(),
+    body('password').isLength({min: 3, max: 32}),
+    userController.registration)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
-router.post('/activate/:link', userController.activate)
-router.post('/refresh', userController.refresh)
-router.get('/users', userController.getUsers)
+router.get('/activate/:link', userController.activate)
+router.get('/refresh', userController.refresh)
+router.get('/users', authMiddleware, userController.getUsers)
 
 export default router
