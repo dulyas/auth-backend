5b269b54d3f0b6fd39d4b4e177568a74376dbafd
ddulyas <120781735+ddulyas@users.noreply.github.com>
2023-03-26 00:43:26 +0500

    add error middleware v2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import express from "express";
 import cors from 'cors'
 import cookieParser from "cookie-parser";
-
 import mongoose from "mongoose";
 import router from "./router/index.js";
-
 import config from "./config/index.js";
+import errorMiddleware from "./middleware/error-middleware.js";
 
 
 
@@ -16,6 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 app.use('/api', router)
+app.use(errorMiddleware)
 
 const start = async () => {
     try {
