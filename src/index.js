ed1bf46a99b50a38f471687ad7d0738bbd1875c7
ddulyas <120781735+ddulyas@users.noreply.github.com>
2023-03-26 20:49:01 +0500

    add cors cfg

--- src/index.js
+++ src/index.js
@@ -13,7 +13,11 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: config.CLIENT_URL
+
+}))
 app.use('/api', router)
 app.use(errorMiddleware)
 
